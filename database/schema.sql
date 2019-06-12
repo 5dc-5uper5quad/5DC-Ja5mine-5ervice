8277b17549339a076b77b04bf9955010ccf8550a
jasminelopez <lopez.jasmine094@gmail.com>
2019-06-12 09:03:47 -0700

    create basic schema without photos available- will integrate the S3 links in the csv file -  create controller files -  create 10M entires in csv file -  successfully setup postgres pool connection -  successully come up with script to copy csv file into postgres

--- database/schema.sql
+++ database/schema.sql
@@ -1,40 +1,33 @@
 DROP DATABASE steam;
 CREATE DATABASE steam;
-/* To connect to the games database */
-\c games;
 
-CREATE games ({
+CREATE TABLE games (
   game_id INTEGER PRIMARY KEY,
   game_name text,
-  game_type: text,
-  original_price: text,
-  sale_boolean: boolean,
-  sale_percentage: text,
-  franchise: boolean,
-  sale_countdown_boolean: boolean,
-  os: [text]
-})
+  game_type text,
+  original_price numeric,
+  reviews text
+);
 
-CREATE dlc ({
-  dlc_name: text,
-  price: text,
-  release_date: Date,
-  user_reviews_overall: text,
-  user_reviews_num: numeric,
-  user_tags: [text],
-  images: [text]
-})
+CREATE TABLE dlc (
+  dlc_name text,
+  price numeric,
+  user_reviews_overall text,
+  user_reviews_num int,
+  user_tags text,
+  images text,
+  game_id INTEGER NOT NULL REFERENCES games(game_id)
+);
 
-
-
-
-
-
-
-
-
+/* 
+ * When first using postgrSQL, you must create a new database cluster in the terminal
+ * 'initdb [option...] [--pgdata | -D] directory' i.e initdb -D SDC 
+ * You can now start the database server using:
+ * pg_ctl -- initialize, start, stop, or control a PostgreSQL server
+ * pg_ctl -D SDC -l logfile start
+ */
 
 
 /*  Execute this file from the command line by typing:
- *   psql -U username -d myDataBase -a -f /database/schema.sql
+ * run \i /Users/Jasmine/hrr38/SDC/5DC-Ja5mine-5ervice/database/schema.sql
  *  to create the database and the tables.*/
