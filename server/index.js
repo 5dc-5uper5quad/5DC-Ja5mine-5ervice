7dfeb06c51c25f0a81af2397ed64ea47116a18c4
jasminelopez <lopez.jasmine094@gmail.com>
2019-06-13 15:29:35 -0700

    complete seeder tha creates CSV file with 10M entires and game_id's -  primarily utilizied the postgreSQL psql shell to COPY the genertated CSV file into the games table -  ran the query for postgreSQL psql shell as well

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 var { Games } = require('../database/index');
+const bodyParser = require('body-parser');
 
 let app = express();
 const port = 3003;
+
 
 app.use(express.static(__dirname+'/../client/dist'));
 
