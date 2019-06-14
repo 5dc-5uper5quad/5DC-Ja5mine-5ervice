e49d96f0f826af4d485db404fa3ae9d896d2b265
jasminelopez <lopez.jasmine094@gmail.com>
2019-06-14 16:19:55 -0700

    begin the initial steps of the refactor by abandoning the use of the json2csv library -  implement a node file stream to add each piece of mockData one by one -  still be in the process of the refactor- still need to implement the same refactor for dlcs

--- database/data/gamesDataGenerator.js
+++ database/data/gamesDataGenerator.js
@@ -1,48 +1,37 @@
 const faker = require('faker');
 const { Parser } = require('json2csv');
 const fs = require("fs");
+const file = fs.createWriteStream('mockDataGames.csv');
 
 const gameTypes = ['farm','fps','car', 'food', 'casino', 'puzzle', 'sports', 'board', 'alien', 'medieval'];
 const OS = ['windows', 'mac', 'linux'];
 const reviewsOverall = ['very positive', 'mostly positive', ' positive', 'mixed', 'negative', 'mostly negative', 'very negative'];
 
-
 //Should create fake data and insert it into the mockData.csv file 
-
-async function createCSVData (iteration) {
+function createCSVData () {
   //create a json object
-  var Games = [];
-  for (let i = 0; i < 50000; i++) {
-    const gameJSON = 
-      {
-        "game_name": faker.commerce.productAdjective() + ' ' + faker.company.catchPhraseNoun(),
-        "game_type": gameTypes[Math.floor(Math.random() * gameTypes.length)],
-        "original_price": faker.commerce.price(0,200,2),
-        "reviews": reviewsOverall[Math.floor(Math.random() * reviewsOverall.length)],
-        "os": OS[Math.ceil(Math.random() * OS.length - 1)],
-      }
-    Games.push(gameJSON);
+  const gameColumns = ['game_id','game_name', 'game_type', 'original_price', 'reviews', 'os'];
+
+  for (let i = 0; i < 100000; i++) { 
+    file.write(`${i},"${faker.commerce.productAdjective() + ' ' + faker.company.catchPhraseNoun()}","${gameTypes[Math.floor(Math.random() * gameTypes.length)]}","${faker.commerce.price(0,200,2)}","${reviewsOverall[Math.floor(Math.random() * reviewsOverall.length)]}","${OS[Math.ceil(Math.random() * OS.length - 1)]}"\n`);
   };
-  //create headers for the csv file and parse to csv
-  const fields = ['game_name', 'game_type', 'original_price', 'reviews', 'os'];
-  const json2csvParser = new Parser({ fields });
-  const csv = json2csvParser.parse(Games);
-  //append csv data into mockData.csv
-  fs.appendFile('mockDataGames.csv', csv, (err) => {
-    if (err) { 
-      console.log(err)
-    } else {
-      console.log("Successfully Written to mockData file!");
-    }
-  });
+    //create headers for the csv file and parse to csv
+  //not worth it
+  //can create this myself CSV 
+  //fs.createWriteStream **
+  //batch it -- generate a string and push it into a file 
+  //line by line 
+  //not it all at once 
+  console.log("CSV files were not successfully created or added");
 };
+
   //promise for csv needs a catch instance
-  createCSVData().then(function (results) {
-    console.log("CSV files successfully created and added!");
-  })
-  .catch(function () {
-    console.log("CSV files were not successfully created or added");
-  });
+  // createCSVData().then(function (results) {
+  //   console.log("CSV files successfully created and added!");
+  // })
+  // .catch(function () {
+  //   console.log("CSV files were not successfully created or added");
+  // });
 
   // add to games table in psql shell `COPY games FROM '/Users/Jasmine/hrr38/SDC/5DC-Ja5mine-5ervice/mockDataGames.csv' DELIMITER ',' CSV HEADER`
   async function batchData () {
@@ -50,7 +39,7 @@ async function createCSVData (iteration) {
     console.log('starting to batch Data....');
     for (let i = 0; i < 1; i++) {
       console.log(i);
-      await createCSVData(i);
+      await createCSVData();
     }
     var millis = Date.now() - start;
     console.log("seconds elapsed = " + Math.floor(millis/1000));
